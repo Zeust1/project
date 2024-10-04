66c31fb8a55d600eab04a6ea267d5b12a66e2716
Zeust1 <hntrung1999@gmail.com>
2024-10-05 00:18:42 +0700

    update location

--- src/components/shopaddress/Shopaddress.jsx
+++ src/components/shopaddress/Shopaddress.jsx
@@ -6,10 +6,15 @@ import shoplocation from "../../../shoplocation.json";
 import showmore from "../../../public/angle-small-down.svg"
 
 const Shopaddress = () => {
-  const [showMoreInfo, setShowMoreInfo] = useState("hide")
 
-  const showMore = () => {
 
+  const showMore = (e) => {
+    const show = document.getElementById(e.target.alt)
+    if (show.style.display === "block") {
+      show.style.display = "none"
+    } else {
+      show.style.display = "block"
+    }
   }
 
 
@@ -17,11 +22,15 @@ const Shopaddress = () => {
     <div className="location-list">
       {shoplocation.map((element) => 
         <div className="location" key={element.id}>
-          <div>
+          <div className="title" >
             <p style={{fontWeight: "bold"}}>{element.name}</p>
             <p style={{fontSize: "12px", fontStyle: "italic"}}>{element.open}</p>
           </div>
-          <img src={showmore} alt="showmore" style={{width: "30px", height: "30px"}}/>
+          <img src={showmore} alt={element.id} style={{width: "30px", height: "30px", display: "flex",justifyContent: "flex-end"}} onClick={showMore}/>
+          <div className="hide" id={element.id}>
+            <p>Add: {element.address}</p>
+            <p>Tel: {element.tel}</p>
+          </div>
         </div>
       )}
     </div>
