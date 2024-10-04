917da796adc6f4a05660688b21132def308f8e95
Trung <hntrung1999@gmail.com>
2024-10-04 19:21:26 +0700

    second

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import Logo from '../public/logo.png'
 import Usericon from '../public/circle-user.svg'
 import Carticon from '../public/shopping-cart-add.svg'
 
+import Shopaddress from './components/shopaddress/Shopaddress'
+
+
+
 function App() {
   const [userIcon, setUserIcon] = useState("hideMenu")
 
@@ -17,6 +21,7 @@ function App() {
   }
 
   return (
+    <>
     <div className='header'>
       <div style={{display: 'flex', alignItems: 'center', gap: '14px'}}>
         <img src={Logo} alt="this logo" style={{width: '50px', height: '50px'}}/>
@@ -25,11 +30,11 @@ function App() {
       <input type="text" placeholder='Bạn cần tìm loại nước uống nào ?'/>
       <div style={{display: 'flex', alignItems: 'center', gap: '14px'}}>
         <div className='cart-icon' style={{width: '30px', height: '30px'}}>
-          <img src={Carticon} alt="user icon" style={{width: '30px', height: '30px'}}/>
+          <img src={Carticon} alt="user icon" style={{width: '30px', height: '30px', cursor: "pointer"}}/>
           <p style={{border: '1px solid', borderRadius: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center', width: '20px', height: '20px', fontWeight: 'bold'}}>1</p>
         </div>
         <div style={{width: '30px', height: '30px'}}>
-          <img src={Usericon} alt="user icon" style={{width: '30px', height: '30px'}} onClick={showMenuUser}/>
+          <img src={Usericon} alt="user icon" style={{width: '30px', height: '30px', cursor: "pointer"}} onClick={showMenuUser}/>
           <div className={userIcon} style={{width: '120px', height: '200px', backgroundColor: 'burlywood'}}>
             <dl>
               <dt>Trang chủ</dt>
@@ -41,6 +46,8 @@ function App() {
         </div>
       </div>
     </div>
+    <Shopaddress/>
+    </>
   )
 }
 
--- src/components/shopaddress/Shopaddress.jsx
+++ src/components/shopaddress/Shopaddress.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import './Shopaddress.css'
+import { useState } from "react";
 
-export const Shopaddress = () => {
-  return (
-    <div>
+import shoplocation from "../../../shoplocation.json";
+import showmore from "../../../public/angle-small-down.svg"
 
+const Shopaddress = () => {
+  const [showMoreInfo, setShowMoreInfo] = useState("hide")
+
+  const showMore = () => {
+
+  }
+
+
+  return(
+    <div className="location-list">
+      {shoplocation.map((element) => 
+        <div className="location" key={element.id}>
+          <div>
+            <p style={{fontWeight: "bold"}}>{element.name}</p>
+            <p style={{fontSize: "12px", fontStyle: "italic"}}>{element.open}</p>
+          </div>
+          <img src={showmore} alt="showmore" style={{width: "30px", height: "30px"}}/>
+        </div>
+      )}
     </div>
   )
 }
 
-export default Shopaddress
+export default Shopaddress;
